321230ca348f9418d58b20f8d476cd47088ed63a
Mateus Serafim <mateuskolap@gmail.com>
2025-03-23 22:57:10 -0300

    Adicionadas funções atualizar, listar e deletar ao Crud de Professores.

--- src/main/java/br/com/mateuskolap/regesc/service/CrudProfessorService.java
+++ src/main/java/br/com/mateuskolap/regesc/service/CrudProfessorService.java
@@ -23,6 +23,9 @@ public class CrudProfessorService {
             System.out.println("Qual ação deseja executar?");
             System.out.println("0 - Voltar");
             System.out.println("1 - Cadastrar novo professor");
+            System.out.println("2 - Atualizar um professor");
+            System.out.println("3 - Visualizar todos os professores");
+            System.out.println("4 - Deletar um professor");
 
             int opcao = scanner.nextInt();
             scanner.nextLine();  // Consome a quebra de linha
@@ -31,7 +34,15 @@ public class CrudProfessorService {
                 case 1:
                     this.cadastrar(scanner);
                     break;
-            
+                case 2:
+                    this.atualizar(scanner);
+                    break;
+                case 3:
+                    this.listar();
+                    break;
+                case 4:
+                    this.deletar(scanner);
+                    break;
                 default:
                     isTrue = false;
                     break;
@@ -51,5 +62,53 @@ public class CrudProfessorService {
         Professor professor = new Professor(nome, prontuario);
         this.professorRepository.save(professor);
         System.out.println("Professor cadastrado com sucesso!\n");
+    }
+
+    private void atualizar(Scanner scanner) {
+        System.out.print("Digite o ID do professor a ser atualizado: ");
+        Long id = scanner.nextLong();
+        scanner.nextLine();  // Consome a quebra de linha
+
+        // Se encontrado o professor com o id passado pelo usuário
+        if (!this.professorRepository.existsById(id)) {
+            System.out.println("ID informado (" + id + ") é inválido!\n");
+            return;
+        }
+
+        Professor professor = this.professorRepository.findById(id).get();
+        
+        System.out.print("Digite o nome do professor: ");
+        String nome = scanner.nextLine();
+        
+        System.out.print("Digite o prontuario do professor: ");
+        String prontuario = scanner.nextLine();
+    
+        professor.setNome(nome);
+        professor.setProntuario(prontuario);
+
+        // Atualiza o professor no banco
+        professorRepository.save(professor);
+
+        System.out.println("Professor atualizado com sucesso.\n");
+    }
+
+    private void listar() {
+        Iterable<Professor> professores = this.professorRepository.findAll();
+
+        professores.forEach(System.out::println);
+        System.out.println();
+    }
+
+    private void deletar(Scanner scanner) {
+        System.out.print("Digite o ID do professor a ser deletado: ");
+        Long id = scanner.nextLong();
+        scanner.nextLine();  // Consome a quebra de linha
+
+        if (this.professorRepository.existsById(id)) {
+            this.professorRepository.deleteById(id);
+            System.out.println("Professor deletado!\n");
+        } else {
+            System.out.println("ID informado (" + id + ") é inválido!\n");
+        }
     }
 }
